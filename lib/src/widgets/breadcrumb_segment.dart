7d9f921fceded88923849a77e6d84e4956bdca35
Seth <sethlamancusa@gmail.com>
2026-08-01 16:12:18 -0400

    .

--- lib/src/widgets/breadcrumb_segment.dart
+++ lib/src/widgets/breadcrumb_segment.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class BreadcrumbSegment extends StatelessWidget {
+class BreadcrumbSegment extends StatefulWidget {
   final String label;
   final VoidCallback onTap;
   final bool isLast;
@@ -15,41 +15,57 @@ class BreadcrumbSegment extends StatelessWidget {
   });
 
   @override
+  State<BreadcrumbSegment> createState() => _BreadcrumbSegmentState();
+}
+
+class _BreadcrumbSegmentState extends State<BreadcrumbSegment> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
+    final shouldHighlight = widget.isDragTarget || _isHovered;
+
     return InkWell(
-      onTap: isLast ? null : onTap,
+      onTap: widget.isLast ? null : widget.onTap,
       borderRadius: BorderRadius.circular(4),
-      child: Container(
-        decoration: isDragTarget
-          ? BoxDecoration(
-              color: Colors.white.withValues(alpha: 0.1),
-              borderRadius: BorderRadius.circular(4),
-            )
-          : null,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                label,
+      onHover: (isHovering) {
+        setState(() => _isHovered = isHovering);
+      },
+      splashColor: shouldHighlight ? Colors.transparent : null,
+      highlightColor: shouldHighlight ? Colors.transparent : null,
+      hoverColor: shouldHighlight ? Colors.transparent : null,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: shouldHighlight
+                ? BoxDecoration(
+                    color: Colors.white.withValues(alpha: 0.1),
+                    borderRadius: BorderRadius.circular(4),
+                  )
+                : null,
+              padding: shouldHighlight ? const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0) : EdgeInsets.zero,
+              child: Text(
+                widget.label,
                 style: TextStyle(
                   fontSize: 14,
-                  color: isLast ? Colors.white : Colors.grey,
-                  fontWeight: isLast ? FontWeight.bold : FontWeight.normal,
+                  color: widget.isLast ? Colors.white : Colors.grey,
+                  fontWeight: widget.isLast ? FontWeight.bold : FontWeight.normal,
                 ),
               ),
-              if (!isLast)
-                const Padding(
-                  padding: EdgeInsets.only(left: 4.0),
-                  child: Icon(
-                    Icons.chevron_right,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
+            ),
+            if (!widget.isLast)
+              const Padding(
+                padding: EdgeInsets.only(left: 4.0),
+                child: Icon(
+                  Icons.chevron_right,
+                  size: 16,
+                  color: Colors.grey,
                 ),
-            ],
-          ),
+              ),
+          ],
         ),
       ),
     );
